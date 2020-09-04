ab92737411976518578b32bc83986ebd438ca008
Tucker <trblackw@ncsu.edu>
2020-09-04 08:50:58 -0400

    init

---
+++ 06-nextjs-auth/01-start/orbit/pages/api/middleware/authorize-admin.js
@@ -0,0 +1,16 @@
+import nextConnect from 'next-connect';
+
+async function authorizeAdmin(req, res, next) {
+  try {
+  } catch (err) {
+    return res
+      .status(401)
+      .json({ message: 'Not authorized' });
+  }
+}
+
+const middleware = nextConnect();
+
+middleware.use(authorizeAdmin);
+
+export default middleware;
